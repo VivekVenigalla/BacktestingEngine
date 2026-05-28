fa50835e69602b4b2e05699e981e2e9d1eb8a8ec
Vivek Venigalla <vivekvenigalla.2008@gmail.com>
2026-05-28 12:03:15 -0400

    Modified main.cpp file for testing and performed unit tests with success

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@
 
 int main(){
     Parser test;
-    std::vector<Bar> newData = test.parse();
-    std::cout<<newData[0].open<<" test";
+    std::map<std::string, Bar> newData = test.parse();
+    std::cout<<newData["2015-01-13 00:00:00-05:00"].open<<" test";
 }

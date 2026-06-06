69e514f6270ac70b9ecf684c98aa0683ba49ac21
Vivek Venigalla <vivekvenigalla.2008@gmail.com>
2026-06-06 12:38:16 -0400

    Made basic class structure and definition

---
+++ include/strategies/smaCross.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "../strategy.hpp"
+
+class smaCross : public Strategy{
+    public:
+        void runBar();
+
+};
---
+++ src/strategies/smaCross.cpp
@@ -0,0 +1,5 @@
+#include "../../include/strategies/smaCross.hpp"
+
+void smaCross::runBar(){
+    //execution here
+}

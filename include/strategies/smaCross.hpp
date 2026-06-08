5e812ca455315d2b2dc6431c2d982977e271dda1
Vivek Venigalla <vivekvenigalla.2008@gmail.com>
2026-06-08 15:06:30 -0400

    Added all methods to smaCross.hpp and .cpp

--- include/strategies/smaCross.hpp
+++ include/strategies/smaCross.hpp
@@ -2,16 +2,32 @@
 #include "../strategy.hpp"
 #include <queue>
 
+
+//NOTES ABOUT STRATEGY
+//This strategy is called a simple moving average
+//implements two windows, one fast and one slow(with the slow window a larger timeframe)
+//when the fast crosses above the slow, buy 20% of current balance in shares(golden cross)
+//vice versa if fast crosses below the slow, sell all shares(death cross)
+//since the fast and slow window will be the same for the first 50 values, there should be no trades occuring then
+//uses market close prices
+//all orders are market
+//since market orders are executed at the open price, all orders are executed the day after the order us creared to prevent look ahead bias
+
 class smaCross : public Strategy{
     public:
+        smaCross(Broker& b, Account& u, Bar& cB, std::unordered_map<long int, Trade>& history, std::string symbol);
+        smaCross(Broker& b, Account& u, Bar& cB, std::unordered_map<long int, Trade>& history, std::string symbol, int fast, int slow);
         void runBar();
+        virtual ~smaCross() = default;
     private:
-        //size_t does not use negative numbers so we have a larger degree of numbers
-        size_t slowLength;
-        size_t fastLength;
+        int slowLength;
+        int fastLength;
 
         double fastSum;
         double slowSum;
+        Order nextOrder;
+        bool check = false;
+        //implementing a queue allows for easy plug in and extraction so we dont have to iterate a lot.
         std::queue<double> fastWindow;
         std::queue<double> slowWindow;
 
--- src/strategies/smaCross.cpp
+++ src/strategies/smaCross.cpp
@@ -1,5 +1,67 @@
 #include "../../include/strategies/smaCross.hpp"
+#include <cmath>
+//cmath includes the operations such as floor
+
+
+smaCross::smaCross(Broker& b, Account& u, Bar& cB, std::unordered_map<long int, Trade>& history, std::string symbol) : broker(b), user(u), connectBar(cB), tradeHistory(history), ticker(symbol), fastLength(50), slowLength(200){
+}
+
+smaCross::smaCross(Broker& b, Account& u, Bar& cB, std::unordered_map<long int, Trade>& history, std::string symbol, int fast, int slow) : broker(b), user(u), connectBar(cB), tradeHistory(history), ticker(symbol), fastLength(fast), slowLength(slow){
+    if (fastLength >= slowLength) {
+        std::cerr << "WARNING: Fast SMA period should be less than Slow SMA period. Adjusting values...\n";
+        fastLength = 50;
+        slowLength = 200;
+    }
+}
+
+
 
 void smaCross::runBar(){
-    //execution here
+    //execute orders if the exist the previous day here
+    if(check){
+        broker.createOrder(nextOrder);
+        check = false;
+    }
+    
+    //add value to both sums and chekc if queues are filled(connectBar will have the most recent Bar)
+    double currPrice = connectBar.close;
+    
+    fastSum += currPrice;
+    slowSum += currPrice;
+
+    fastWindow.push(currPrice);
+    slowWindow.push(currPrice);
+    
+    //check if the queues are filled up and pop if necessary
+    if(fastWindow.size() > fastLength){
+        fastSum -= fastWindow.front();
+        fastWindow.pop();
+    }
+    if(slowWindow.size() > slowLength){
+        slowSum -= slowWindow.front();
+        slowWindow.pop();
+    }
+
+    //if both windows are filled then execute sma logic
+    if(fastWindow.size() == fastLength && slowWindow.size() == slowLength){
+        //calculuate averages
+        double fastAverage = fastSum/fastLength;
+        double slowAverage = slowSum/slowLength;
+
+        //check if fast is larger than slow => golden cross
+        if(fastAverage > slowAverage){
+            double currBalance = user.checkBalance();
+            //calculate 20% of currBalance worth in shares(floored in order to have int shares)
+            long numShares = std::floor((currBalance*0.2)/currPrice);
+            //create Order struct
+            nextOrder = {ticker, "market", 0, numShares, -1.0};
+            check = true;
+        }
+        else if(fastAverage < slowAverage){
+            long numShares = user.checkPosition(ticker);
+            nextOrder = {ticker, "market", 1, numShares, -1.0};
+            check = true;
+        }
+    }
+    
 }
